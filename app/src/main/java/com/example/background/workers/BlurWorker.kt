ab4ec28233826eed54c497096865bbde245dce1c
Falchio <53504233+Falchio@users.noreply.github.com>
2021-04-17 12:04:31 +0300

    before add new Workers

---
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -0,0 +1,43 @@
+package com.example.background.workers
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.text.TextUtils
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import androidx.work.workDataOf
+import com.example.background.KEY_IMAGE_URI
+import com.example.background.R
+import timber.log.Timber
+
+class BlurWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
+    override fun doWork(): Result {
+
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+        makeStatusNotification("Blurring image", applicationContext)
+
+        return try {
+            if (TextUtils.isEmpty(resourceUri)) {
+                Timber.e("Invalid input uri")
+                throw IllegalArgumentException("Invalid input uri")
+            }
+
+            val resolver = applicationContext.contentResolver
+
+            val picture = BitmapFactory.decodeStream(
+                    resolver.openInputStream(Uri.parse(resourceUri)))
+
+
+            val output = blurBitmap(picture, applicationContext)
+            val outputUri = writeBitmapToFile(applicationContext, output)
+            makeStatusNotification("Output is $outputUri", applicationContext)
+            val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+            Result.success(outputData)
+        } catch (throwable: Throwable) {
+            Timber.e(throwable, "Error applying blur")
+            Result.failure()
+        }
+
+    }
+}
